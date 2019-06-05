b574232b4f82c32b5206507a63198e2af2d06c72
Nathan Mass <30848495+nathanmass1@users.noreply.github.com>
2019-06-05 16:57:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,39 @@
-$(function () {
-
-  $(".btn-primary").on("click", function (evt) {
-    evt.preventDefault();
-    let $query = $(".search-bar").val();
+const $gifArea = $("#gif-area");
+const $searchInput = $("#search");
 
 
-    let gif = getGif($query);
-    $("form").trigger("reset");
-    // showGif(gif)
+/* use ajax result to add a gif */
 
-  });
-
-  $(".btn-danger").on("click", function (evt) {
-    evt.preventDefault();
-    let ele = document.querySelector(".gif-div");
-    clearDomElement(ele);
-  });
-
-  function clearDomElement(ele) {
-    while (ele.firstChild) {
-      ele.removeChild(ele.firstChild);
-    }
-  }
-
-
-  async function getGif(query) {
-    let apiURL = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=G8vYr8T6crBB6uv6azGMtVDn2RLbmK6T`;
-    await $.get(apiURL, function (res) {
-      showGif(res.data[0].images.original.url);
-      // console.log(res.data[0].images.original.url);
-      // return ;
+function addGif(res) {
+  let numResults = res.data.length;
+  if (numResults) {
+    let randomIdx = Math.floor(Math.random() * numResults);
+    let $newCol = $("<div>", { class: "col-md-4 col-12 mb-4" });
+    let $newGif = $("<img>", {
+      src: res.data[randomIdx].images.original.url,
+      class: "w-100"
     });
+    $newCol.append($newGif);
+    $gifArea.append($newCol);
   }
+}
 
-  function showGif(imgLink) {
-    const $gifDiv = $('.gif-div');
-    let gifCard = $(
-      `<div class="col-md-4">
-                <div>
-                    <img src="${imgLink}" alt="">
-                </div>
-            </div>`
-    );
-    $gifDiv.append(gifCard);
-  }
+/* handle form submission: clear search box & make ajax call */
 
+$("form").on("submit", function (e) {
+  e.preventDefault();
 
+  let searchTerm = $searchInput.val();
+  $searchInput.val("");
+
+  $.get(
+    "http://api.giphy.com/v1/gifs/search",
+    { q: searchTerm, api_key: "G8vYr8T6crBB6uv6azGMtVDn2RLbmK6T" },
+    addGif);
+});
+
+/* remove gif */
+
+$("#remove").on("click", function () {
+  $gifArea.empty();
 });
